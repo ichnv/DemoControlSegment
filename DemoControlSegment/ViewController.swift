f8f6e836a8afddf2141532acd1dca3db213e134b
ichNV-D1-M <ichnv@rikkeisoft.com>
2020-03-29 23:10:15 +0700

    Initial Commit

---
+++ DemoControlSegment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoControlSegment
+//
+//  Created by Mr Ích on 3/29/20.
+//  Copyright © 2020 Mr Ích. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
